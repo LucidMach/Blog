e04ba2ac234729fc7c177b22e69591b653e2ea23
lucidmach <lucidmach@gmail.com>
2023-09-29 13:06:14 +0530

    de-ml

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Ziro to Mach";
-export const SITE_DESCRIPTION = 'my learning "machine learning" blog';
+export const SITE_TITLE = "Ziro2Mach";
+export const SITE_DESCRIPTION = "dream it... build it!";
