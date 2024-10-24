0c13fc2f04d2026c2384bde8af101eb4616806f3
lucidmach <lucidmach@gmail.com>
2024-10-24 19:39:10 +1100

    vercel adapter to upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import db from "@astrojs/db";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ziro2mach.com",
   integrations: [mdx(), sitemap(), tailwind(), db(), react()],
   output: "server",
+  adapter: vercel(),
 });
