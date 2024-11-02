22feb861c28ecf26ac1f3a2acf87081a2c610c41
lucidmach <lucidmach@gmail.com>
2024-11-03 08:05:46 +1100

    trying without serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // import vercel from "@astrojs/vercel/serverless";
 
@@ -14,9 +11,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: "https://ziro2mach.com",
   integrations: [mdx(), sitemap(), tailwind(), db(), react()],
-
-  // adapter: vercel(),
   output: "server",
-
-  adapter: vercel(),
+  // adapter: vercel(),
 });
