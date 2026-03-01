aa7cd4ea509d8bb2cef77f71b271933a643636a3
lucidmach <lucidmach@gmail.com>
2026-03-01 15:47:55 +1100

    back to a static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import db from "@astrojs/db";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ziro2mach.com",
-  integrations: [mdx(), sitemap(), tailwind(), db(), react()],
-  output: "server",
-  adapter: vercel(),
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "static",
 });
